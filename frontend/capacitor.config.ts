ac797ddd037578a62edf74d5415048fc3c529c91
Marwan1911UWK <150447688+Marwan1911UWK@users.noreply.github.com>
2026-06-28 14:51:15 +0300

    build: add Capacitor to generate Android APK

---
+++ frontend/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.autocare.app',
+  appName: 'auto-CARe',
+  webDir: 'dist'
+};
+
+export default config;
